e17ab4e2018100fce71a8b9f555a8e831086c8c7
wizfury <21052721@kiit.ac.in>
2024-08-03 11:25:12 +0530

    feat: queue

--- new/interviewQs/partition.py
+++ new/interviewQs/partition.py
@@ -1,13 +1,18 @@
 from linkedlist import LinkedList
 
 def partition(ll,x):
-    curNode= ll.head
-    ll.tail=curNode
+    curnode = ll.head
+    ll.tail = ll.head
     
-    while curNode:
+    while curnode:
+        newNode = curnode.next
+        curnode.next = None
+        if curnode.data < x:
+            curnode.next = ll.head
+            ll.head = curnode
+        else:
+            ll.tail.next = curnode
+            ll.tail = curnode
+        curnode = curnode.next
+            
         
-        
-    
-    
-        
-    
